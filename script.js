b2343d7493ec9990f9c5b2073aef61b52d7519cc
Eileen Chang <chng.eiln@gmail.com>
2023-04-01 01:31:44 -0400

    house rooms

--- script.js
+++ script.js
@@ -17,28 +17,56 @@ const livingRoom = {
   image: new Image()
 };
 
+const bedroom = {
+    x: 0,
+    y: 0,
+    width: canvas.width,
+    height: canvas.height,
+    image: new Image()
+  };
+
 house.image.src = "images/house.jpg";
 house.image.addEventListener("load", draw);
 
 livingRoom.image.src = "images/living-room.jpg";
 livingRoom.image.addEventListener("load", draw);
 
+bedroom.image.src = "images/bedroom.jpg";
+bedroom.image.addEventListener("load", draw);
+
 let gameState = "outside";
 
 function drawHouse() {
-  ctx.drawImage(house.image, house.x, house.y, house.width, house.height);
+    ctx.drawImage(house.image, house.x, house.y, house.width, house.height);
+}
+  
+function drawLivingRoom() {
+ctx.drawImage(livingRoom.image, livingRoom.x, livingRoom.y, livingRoom.width, livingRoom.height);
 }
 
-function drawLivingRoom() {
-  ctx.drawImage(livingRoom.image, livingRoom.x, livingRoom.y, livingRoom.width, livingRoom.height);
+function drawBedroom() {
+    ctx.drawImage(bedroom.image, bedroom.x, bedroom.y, bedroom.width, bedroom.height);
 }
 
 const leaveHouseBtn = document.getElementById("leaveHouseBtn");
+
+
 leaveHouseBtn.addEventListener("click", () => {
   gameState = "outside";
   leaveHouseBtn.style.display = "none";
   draw();
 }); 
+
+const leaveBedroomBtn = document.createElement("button");
+leaveBedroomBtn.textContent = "Leave the Bedroom";
+leaveBedroomBtn.style.display = "none";
+document.body.appendChild(leaveBedroomBtn);
+
+leaveBedroomBtn.addEventListener("click", () => {
+  gameState = "livingRoom";
+  leaveBedroomBtn.style.display = "none";
+  draw();
+});
 
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -47,6 +75,8 @@ function draw() {
     drawHouse();
   } else if (gameState === "livingRoom") {
     drawLivingRoom();
+  } else if (gameState === "bedroom") {
+    drawBedroom();
   }
 }
 
@@ -58,6 +88,12 @@ canvas.addEventListener("click", (event) => {
   if (gameState === "outside" && x >= house.x && x <= house.x + house.width && y >= house.y && y <= house.y + house.height) {
     gameState = "livingRoom";
     leaveHouseBtn.style.display = "block";
+    draw();
+  }
+  else if (gameState === "livingRoom" && x >= bedroom.x && x <= bedroom.x + bedroom.width && y >= bedroom.y && y <= bedroom.y + bedroom.height) {
+    gameState = "bedroom";
+    leaveHouseBtn.style.display = "block";
+    leaveBedroomBtn.style.display = "block";
     draw();
   }
 });
